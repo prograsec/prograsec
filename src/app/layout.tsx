e65b4e8b6d92aefd0dd7894438bd457ca25cbe7e
usama-365 <usama-365@protonmail.com>
2024-02-19 15:11:49 +0500

    setup nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
+import "@/app/globals.css";
+import { Providers } from "@/app/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
-  return <p className="text-red-500">Hello World</p>;
+  return (
+    <>
+      <p className="text-red-500">Hello Tailwind</p>
+      <Button>Hello NextUI</Button>
+    </>
+  );
 }
